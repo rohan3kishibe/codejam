40620523de7114523bff98eb07e323e5ca81e20c
rohan <sagnikchatterjee24@gmail.com>
2021-08-02 08:37:34 +0530

    init

---
+++ 2016/qualification/countingSheep.py
@@ -0,0 +1,15 @@
+def solution(n):
+    pass
+
+
+
+def main():
+    t=int(input())
+    while(t>0):
+        n=int(input())
+        result = solution(n)
+        t=t-1
+
+
+if __name__=='__main__':
+    main()
